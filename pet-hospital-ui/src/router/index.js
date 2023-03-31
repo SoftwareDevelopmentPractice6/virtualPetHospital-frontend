c9ce8325c06c87cf874210038eb7c5ea470deb44
zoella2021 <2695969122@qq.com>
2023-03-31 08:08:14 +0800

    3D医院地图

--- pet-hospital-ui/src/router/index.js
+++ pet-hospital-ui/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Cookies from "js-cookie";
 import Layout from "@/layout";
 
@@ -55,7 +55,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
