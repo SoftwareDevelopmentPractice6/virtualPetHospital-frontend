ea38b8bfb3011010cfd85c0d1dbc4c56054c4b35
“yiyiyijiajiajia” <10195101471@stu.ecnu.edu.cn>
2023-03-31 08:33:52 +0800

    更新了学生测试部分，包括考试查询，考试作答和成绩查询等

---
+++ pet-hospital-ui/src/views/StudentExam/ExamContent.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="ExamContent">
+		<h1 class="title">试卷内容</h1>
+		<div class="Select">
+			<h2 class="title1">一、选择题</h2>
+			<el-row>
+				<el-text class="selection1"
+					>1、第一个选择题巴拉巴拉巴拉不知道是什么题</el-text
+				></el-row
+			>
+			<el-radio-group class="seleAnswer1" v-model="radio">
+				<el-radio :label="1">A.第一个选项阿巴阿巴</el-radio>
+				<el-radio :label="2">B.第二个选项阿巴阿巴</el-radio>
+				<el-radio :label="3">C.第三个选项阿巴阿巴</el-radio>
+			</el-radio-group>
+		</div>
+		<div class="Essay">
+			<h2 class="title2">二、简答题</h2>
+			<el-row>
+				<el-text class="essay1"
+					>1、第一个简答题题干信息巴拉巴拉</el-text
+				></el-row
+			>
+			<el-input
+				class="essayAnswer1"
+				v-model="textarea"
+				:rows="3"
+				type="textarea"
+				placeholder="Please input"
+			/>
+		</div>
+		<div class="button">
+			<router-link to="/SubmitSuccess">
+				<el-button class="submitButton" type="primary"
+					>提交</el-button
+				></router-link
+			>
+			<router-link to="/ExamSelection">
+				<el-button class="cancelButton">取消</el-button></router-link
+			>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const radio = ref(3);
+const textarea = ref("");
+</script>
+
+<style lang="scss" scoped>
+.title {
+	margin: 30px auto 30px auto;
+	text-align: center;
+}
+
+.Select {
+	margin: 30px 30px 30px 30px;
+}
+
+.Essay {
+	margin: 30px 30px 30px 30px;
+}
+
+.essayAnswer1 {
+	margin: 20px auto 20px auto;
+}
+
+.button {
+	text-align: center;
+	margin-block-end: 10px;
+}
+.submitButton {
+	width: 150px;
+	height: 40px;
+	margin: auto 100px auto auto;
+}
+
+.cancelButton {
+	width: 150px;
+	height: 40px;
+}
+</style>
---
+++ pet-hospital-ui/src/views/StudentExam/SubmitSuccess.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="SubmitSuccess">
+		<div class="submit">
+			<el-icon class="icon" style="large"><SuccessFilled /></el-icon>
+			<h1 class="title">提交成功</h1>
+		</div>
+		<div>
+			<router-link to="/ExamSelection">
+				<el-button type="success" round class="finish">完成</el-button></router-link
+			>
+		</div>
+	</div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.SubmitSuccess {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	flex-direction: column;
+	.submit {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap; /* 将组件1和组件2放在同一行 */
+		max-width: 30%; /* 设置行的最大宽度 */
+	}
+	.icon,
+	.title{
+		margin: 10px; /* 设置组件之间的间距 */
+		width: 130px; /* 设置组件的宽度 */
+		height: 100px; /* 设置组件的高度 */
+		display: flex; /* 将组件内的内容垂直水平居中 */
+		justify-content: center;
+		align-items: center;
+
+        .icon{
+            width:50px;
+            height:50px;
+        }
+	}
+	.finish {
+		margin-top: 10px; /* 设置组件3与组件1和组件2之间的间距 */
+		width: 150px;
+		height: 40px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+}
+</style>
