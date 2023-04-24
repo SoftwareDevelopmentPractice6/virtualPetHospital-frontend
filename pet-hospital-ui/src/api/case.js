4dabd4cda8ba02202bed931aa01a626ed3491d09
zoella2021 <2695969122@qq.com>
2023-04-25 03:25:11 +0800

    病例管理页结构样式重写 + 增加查看病例功能

--- pet-hospital-ui/src/api/case.js
+++ pet-hospital-ui/src/api/case.js
@@ -6,92 +6,84 @@ const base = "disease/";
 
 // 获取所有病种
 export function getDisease() {
-	return request({
-		url: base + "disease",
-		method: "get",
-	});
+  return request({
+    url: base + "disease",
+    method: "get",
+  });
 }
 
 // 获取所有病例
 export function getCase(
-	admissionKeyword,
-	caseCheckKeyword,
-	diagnosticResultKeyword,
-	treatmentProgramKeyword,
-	diseaseNameId
+  admissionKeyword,
+  caseCheckKeyword,
+  diagnosticResultKeyword,
+  treatmentProgramKeyword,
+  diseaseNameId
 ) {
-	return request({
-		url: base + "medical-case",
-		method: "get",
-		params: {
-			admissionKeyword,
-			caseCheckKeyword,
-			diagnosticResultKeyword,
-			treatmentProgramKeyword,
-			diseaseNameId,
-		},
-	});
+  return request({
+    url: base + "medical-case",
+    method: "get",
+    params: {
+      admissionKeyword,
+      caseCheckKeyword,
+      diagnosticResultKeyword,
+      treatmentProgramKeyword,
+      diseaseNameId,
+    },
+  });
 }
 
-//根据疾病名称关键词搜索
-export function getMedicineByKeyword(diseaseNameKeyword) {
-	return request({
-		url: base + `disease/${diseaseNameKeyword}`,
-		method: "get",
-	});
+// 根据疾病名称关键词搜索
+export function getMedicineByKeyword(diseaseNameKeyword, diseaseNameCategory) {
+  return request({
+    url: base + "disease",
+    method: "get",
+    params: {
+      diseaseNameKeyword,
+      diseaseNameCategory,
+    },
+  });
 }
-//根据药品名称获取药品信息
-/* export function getCaseByName(medicineName) {
-	return request({
-		url: base + `medical-case/${medicineName}`,
-		method: "get",
-	});
-} */
-// 新增
+
+// 根据药品名称获取药品信息
+export function getCaseByName(medicineName) {
+  return request({
+    url: base + `medical-case/${medicineName}`,
+    method: "get",
+  });
+}
+
+// 新增病例
 export function insertCase(val) {
-	return request({
-		url: base + "medical-case",
-		method: "post",
-		data: val,
-	});
+  return request({
+    url: base + "medical-case",
+    method: "post",
+    data: val,
+  });
 }
-export function insertDisease(val) {
-	return request({
-		url: base + "medical-case",
-		method: "post",
-		data: val,
-	});
-}
-//更改药品信息
+
+// 更改药品信息
 export function updateCase(val) {
-	return request({
-		url: base + "medical-case",
-		method: "put",
-		data: val,
-	});
+  return request({
+    url: base + "medical-case",
+    method: "put",
+    data: val,
+  });
 }
-//删除疾病信息
+
+// 删除疾病信息
 export function deleteDiseaseById(diseaseNameId) {
-	return request({
-		url: base + `disease/${diseaseNameId}`,
-		method: "delete",
-	});
+  return request({
+    url: base + `disease/${diseaseNameId}`,
+    method: "delete",
+  });
 }
 
-//获取病例信息
-export function getMedicalCase(params) {
-	return request({
-		url: base + `medical-case`,
-		method: "get",
-		params,
-	});
-}
-
-//保存病例信息------接口跨域
+// 保存病例信息------接口跨域
 export function putMedicalCase(data) {
-	return request({
-		url: base + `medical-case `,
-		method: "put",
-		data,
-	});
+  return request({
+    url: base + `medical-case `,
+    method: "put",
+    data,
+  });
 }
