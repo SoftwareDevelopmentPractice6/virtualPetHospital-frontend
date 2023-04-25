e982c4cbccaa730f8108d3a5c17cc3cda6a06f86
zoella2021 <2695969122@qq.com>
2023-04-25 22:58:31 +0800

    病例编辑页

--- pet-hospital-ui/src/views/Cases/Detail.vue
+++ pet-hospital-ui/src/views/Cases/Detail.vue
@@ -5,36 +5,42 @@
         <el-button type="plain" @click="cancel">返回</el-button>
       </div>
     </div>
-    <div class="title-wrapper">
-      <div class="title-container"></div>
-    </div>
     <div class="main-wrapper">
-      <div class="demo-collapse">
+      <div class="form-container">
+        <div class="item diseaseCategory">
+          疾病类型: <span>{{ curDisease.diseaseNameCategory }}</span>
+        </div>
+        <div class="item diseaseName">
+          疾病名称: <span>{{ curDisease.diseaseNameContent }}</span>
+        </div>
+      </div>
+      <div class="collapse-container">
         <el-collapse v-model="activeNames">
-          <el-collapse-item title="接诊" name="接诊">
-            <InputFile :filePath="filePaths.photos[0]" />
-          </el-collapse-item>
-          <el-collapse-item title="病例检查" name="病例检查">
-            <InputFile :filePath="filePaths.photos[1]" />
-          </el-collapse-item>
-          <el-collapse-item title="诊断结果" name="诊断结果">
-            <InputFile :filePath="filePaths.photos[2]" />
-          </el-collapse-item>
-          <el-collapse-item title="治疗方案" name="治疗方案">
-            <InputFile :filePath="filePaths.photos[3]" />
+          <el-collapse-item
+            v-for="(item, index) in submitInfo"
+            :key="index"
+            :title="item.title"
+            :name="item.title"
+          >
+            <InputFile
+              :filePath="[item.photo, item.video]"
+              v-model:value="item.value"
+            />
           </el-collapse-item>
         </el-collapse>
       </div>
     </div>
     <div class="btns-wrapper">
-      <el-button type="primary" @click="onSubmit"> 保存 </el-button>
-      <el-button @click="cancel">取消</el-button>
+      <div class="btns-container">
+        <el-button type="primary" @click="onSubmit">保存</el-button>
+        <el-button @click="cancel">取消</el-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import InputFile from "./InputFile.vue";
 import { getDiseaseById, getCaseById, updateCase } from "@/api/case";
 import { useRoute, useRouter } from "vue-router";
@@ -47,10 +53,7 @@ const diseaseNameId = route.query.diseaseNameId,
 
 const curDisease = ref({}),
   curCase = ref([]);
-const filePaths = reactive({
-  photos: [],
-  videos: [],
-});
+const submitInfo = ref([]);
 
 const initData = async () => {
   await getDiseaseById(diseaseNameId).then((res) => {
@@ -60,19 +63,31 @@ const initData = async () => {
     curCase.value = res.data;
   });
   const caseVal = curCase.value;
-  filePaths.photos = [
-    caseVal.medicalCaseAdmission.admissionPhoto,
-    caseVal.medicalCaseCaseCheck.caseCheckPhoto,
-    caseVal.medicalCaseDiagnosticResult.diagnosticResultPhoto,
-    caseVal.medicalCaseTreatmentProgram.treatmentProgramPhoto,
-  ];
-  filePaths.videos = [
-    caseVal.medicalCaseAdmission.admissionVideo,
-    caseVal.medicalCaseCaseCheck.caseCheckVideo,
-    caseVal.medicalCaseDiagnosticResult.diagnosticResultVideo,
-    caseVal.medicalCaseTreatmentProgram.treatmentProgramVideo,
-  ];
-  console.log("filePaths.value", filePaths.value);
+  submitInfo.value.push({
+    title: "接诊",
+    photo: caseVal.medicalCaseAdmission.admissionPhoto,
+    video: caseVal.medicalCaseAdmission.admissionVideo,
+    value: caseVal.medicalCaseAdmission.admissionContent,
+  });
+  submitInfo.value.push({
+    title: "病例检查",
+    photo: caseVal.medicalCaseCaseCheck.caseCheckPhoto,
+    video: caseVal.medicalCaseCaseCheck.caseCheckVideo,
+    value: caseVal.medicalCaseCaseCheck.caseCheckContent,
+  });
+  submitInfo.value.push({
+    title: "诊断结果",
+    photo: caseVal.medicalCaseDiagnosticResult.diagnosticResultPhoto,
+    video: caseVal.medicalCaseDiagnosticResult.diagnosticResultVideo,
+    value: caseVal.medicalCaseDiagnosticResult.diagnosticResultContent,
+  });
+  submitInfo.value.push({
+    title: "治疗方案",
+    photo: caseVal.medicalCaseTreatmentProgram.treatmentProgramPhoto,
+    video: caseVal.medicalCaseTreatmentProgram.treatmentProgramVideo,
+    value: caseVal.medicalCaseTreatmentProgram.treatmentProgramContent,
+  });
+  console.log("submitInfo.value", submitInfo.value);
 };
 
 onMounted(() => {
@@ -80,15 +95,6 @@ onMounted(() => {
 });
 
 const activeNames = ref(["接诊"]); // 展开项
-
-const submitInfo = reactive({
-  medicalCaseAdmission: {},
-  medicalCaseCaseCheck: {},
-  medicalCaseDiagnosticResult: {},
-  medicalCaseDiseaseName: {},
-  medicalCaseTreatmentProgram: {},
-  medicalCaseId: "",
-});
 
 const getMedicalCaseData = async () => {};
 getMedicalCaseData();
@@ -101,12 +107,10 @@ const onSubmit = async () => {
   const info = {
     medicalCaseId: route.query.medicalCaseId,
     diseaseNameId: route.query.diseaseNameId,
-    admissionContent: submitInfo.medicalCaseAdmission.admissionContent,
-    caseCheckContent: submitInfo.medicalCaseCaseCheck.caseCheckContent,
-    diagnosticResultContent:
-      submitInfo.medicalCaseDiagnosticResult.diagnosticResultContent,
-    treatmentProgramContent:
-      submitInfo.medicalCaseTreatmentProgram.treatmentProgramContent,
+    admissionContent: submitInfo.value[0].value,
+    caseCheckContent: submitInfo.value[1].value,
+    diagnosticResultContent: submitInfo.value[2].value,
+    treatmentProgramContent: submitInfo.value[3].value,
   };
   updateCase(info);
   cancel();
@@ -115,15 +119,37 @@ const onSubmit = async () => {
 
 <style lang="scss" scoped>
 .casedetail-wrapper {
-  padding: 10px 25px;
+  overflow: auto;
+  height: calc(100vh - 50px);
+  padding: 10px;
   .header-wrapper {
-    margin-bottom: 5px;
+    margin-bottom: 20px;
   }
   .main-wrapper {
+    margin: 0 40px 20px;
+    .form-container {
+      margin-bottom: 15px;
+      .item {
+        margin-bottom: 10px;
+        font-weight: 600;
+        span {
+          font-weight: 500;
+        }
+      }
+    }
+  }
+  .btns-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .btns-container {
+      width: 140px;
+    }
   }
 }
 
 ::v-deep .el-collapse-item__header {
-  font-size: 20px;
+  font-size: 18px;
+  font-weight: 600;
 }
 </style>
