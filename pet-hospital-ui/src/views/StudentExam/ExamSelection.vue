9aac0203e4326f93161bf583e7668b69fcc2e1ee
zoella2021 <2695969122@qq.com>
2023-04-26 13:50:50 +0800

    修用户管理 & 测试管理 bugs

--- pet-hospital-ui/src/views/StudentExam/ExamSelection.vue
+++ pet-hospital-ui/src/views/StudentExam/ExamSelection.vue
@@ -1,67 +1,67 @@
 <template>
-	<div class="case-wrapper">
-		<div class="case-container">
-			<!-- 模糊搜索框 -->
-			<div class="header-wrapper wrapper">
-				<el-form :inline="true" :model="examlist" class="search">
-					<el-form-item label="考试名称">
-						<el-input v-model="examlist.examname" placeholder="考试名称" />
-					</el-form-item>
-					<el-form-item>
-						<el-button type="primary" @click="onSubmit">搜索</el-button>
-						<el-button type="info" @click="resetForm">重置</el-button>
-					</el-form-item>
-				</el-form>
-			</div>
+  <div class="case-wrapper">
+    <div class="case-container">
+      <!-- 模糊搜索框 -->
+      <div class="header-wrapper wrapper">
+        <el-form :inline="true" :model="examlist" class="search">
+          <el-form-item label="考试名称">
+            <el-input v-model="examlist.examname" placeholder="考试名称" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">搜索</el-button>
+            <el-button type="info" @click="resetForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
 
-			<div class="table-wrapper wrapper">
-				<div class="table-container">
-					<el-table
-						ref="multipleTableRef"
-						:data="tableData"
-						height="400px"
-						:header-cell-style="{ 'text-align': 'center' }"
-						:cell-style="{ 'text-align': 'center' }"
-						@selection-change="handleSelectionChange"
-					>
-						<el-table-column type="selection" width="55" />
-						<el-table-column type="index" label="序号" width="55" />
+      <div class="table-wrapper wrapper">
+        <div class="table-container">
+          <el-table
+            ref="multipleTableRef"
+            :data="tableData"
+            height="400px"
+            :header-cell-style="{ 'text-align': 'center' }"
+            :cell-style="{ 'text-align': 'center' }"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55" />
+            <el-table-column type="index" label="序号" width="55" />
 
-						<el-table-column prop="examname" label="考试名称" width="120" />
-						<el-table-column prop="examduration" label="考试时长" width="110" />
-						<el-table-column
-							prop="examtotalscore"
-							label="考试总分"
-							width="110"
-						/>
-						<el-table-column
-							prop="examstart"
-							label="考试开始时间"
-							width="170"
-						/>
-						<el-table-column prop="examend" label="考试结束时间" width="170" />
-						<el-table-column
-							prop="papername"
-							label="考试试卷名称"
-							width="120"
-						/>
+            <el-table-column prop="examname" label="考试名称" width="120" />
+            <el-table-column prop="examduration" label="考试时长" width="110" />
+            <el-table-column
+              prop="examtotalscore"
+              label="考试总分"
+              width="110"
+            />
+            <el-table-column
+              prop="examstart"
+              label="考试开始时间"
+              width="170"
+            />
+            <el-table-column prop="examend" label="考试结束时间" width="170" />
+            <el-table-column
+              prop="papername"
+              label="考试试卷名称"
+              width="120"
+            />
 
-						<el-table-column label="操作" width="200">
-							<template #default="scope">
-								<el-button
-									size="small"
-									type="warning"
-									plain
-									@click="handleEdit(scope.row)"
-									>开始考试</el-button
-								>
-							</template>
-						</el-table-column>
-					</el-table>
-				</div>
-			</div>
-		</div>
-	</div>
+            <el-table-column label="操作" width="200">
+              <template #default="scope">
+                <el-button
+                  size="small"
+                  type="warning"
+                  plain
+                  @click="handleEdit(scope.row)"
+                  >开始考试</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import { onMounted, reactive, ref } from "vue";
@@ -72,124 +72,121 @@ const loading = ref();
 
 // let tableData = reactive([]);
 const examlist = reactive({
-	id: "",
-	examname: "",
-	examrealid: "",
-	examduration: "",
-	examtotalscore: "",
-	examstart: "",
-	examend: "",
-	papername: "",
+  id: "",
+  examname: "",
+  examrealid: "",
+  examduration: "",
+  examtotalscore: "",
+  examstart: "",
+  examend: "",
+  papername: "",
 });
 const tableData = ref([]);
 
 onMounted(() => {
-	getAll();
+  getAll();
 });
 const handleSelectionChange = (val) => {
-	console.log(val);
+  console.log(val);
 };
 // 获取全部信息
 const getAll = async () => {
-	tableData.value = [];
-	loading.value = true;
-	let data = await getExamList().then((res) => res.data);
-	data.examSessionList.forEach((item) => {
-		var value = {
-			id: item.examSessionId,
-			examname: item.examSessionPaper.paperExam.examName,
-			examrealid: item.examSessionPaper.paperExam.examId,
-			examduration: item.examSessionPaper.paperDuration,
-			examtotalscore: item.examSessionPaper.paperTotalScore,
-			examstart: item.examSessionStartTime,
-			examend: item.examSessionEndTime,
-			papername: item.examSessionPaper.paperName,
-		};
-		tableData.value.push(value);
-	});
-	console.log("tabledata", tableData);
+  tableData.value = [];
+  loading.value = true;
+  let data = await getExamList().then((res) => res.data);
+  data.examSessionList.forEach((item) => {
+    var value = {
+      id: item.examSessionId,
+      examname: item.examSessionPaper.paperExam.examName,
+      examrealid: item.examSessionPaper.paperExam.examId,
+      examduration: item.examSessionPaper.paperDuration,
+      examtotalscore: item.examSessionPaper.paperTotalScore,
+      examstart: item.examSessionStartTime,
+      examend: item.examSessionEndTime,
+      papername: item.examSessionPaper.paperName,
+    };
+    tableData.value.push(value);
+  });
+  console.log("tabledata", tableData);
 };
 
 const onSubmit = async () => {
-	// console.log("submit!");
-	if (examlist.examname === "") return;
-	tableData.value = [];
-	loading.value = true;
-	const data = await getExamByName(examlist.examname).then((res) => res.data);
-	data.examSessionList.forEach((item) => {
-		var value = {
-			examname: item.examSessionPaper.paperExam.examName,
-			examrealid: item.examSessionPaper.paperExam.examId,
-			examduration: item.examSessionPaper.paperDuration,
-			examtotalscore: item.examSessionPaper.paperTotalScore,
-			examstart: item.examSessionStartTime,
-			examend: item.examSessionEndTime,
-			papername: item.examSessionPaper.paperName,
-		};
-		tableData.value.push(value);
-	});
-	loading.value = false;
+  // console.log("submit!");
+  if (examlist.examname === "") return;
+  tableData.value = [];
+  loading.value = true;
+  const data = await getExamByName(examlist.examname).then((res) => res.data);
+  data.examSessionList.forEach((item) => {
+    var value = {
+      examname: item.examSessionPaper.paperExam.examName,
+      examrealid: item.examSessionPaper.paperExam.examId,
+      examduration: item.examSessionPaper.paperDuration,
+      examtotalscore: item.examSessionPaper.paperTotalScore,
+      examstart: item.examSessionStartTime,
+      examend: item.examSessionEndTime,
+      papername: item.examSessionPaper.paperName,
+    };
+    tableData.value.push(value);
+  });
+  loading.value = false;
 
-	console.log("tabledata", tableData);
+  console.log("tabledata", tableData);
 };
 
 const resetForm = () => {
-	examlist.id = "";
-	examlist.examname = "";
-	examlist.examrealid = "";
-	examlist.examduration = "";
-	examlist.examtotalscore = "";
-	examlist.examstart = "";
-	examlist.examend = "";
-	examlist.papername = "";
+  examlist.id = "";
+  examlist.examname = "";
+  examlist.examrealid = "";
+  examlist.examduration = "";
+  examlist.examtotalscore = "";
+  examlist.examstart = "";
+  examlist.examend = "";
+  examlist.papername = "";
 };
 
 const handleEdit = (examSessionList) => {
-	const examrealid = examSessionList.examrealid;
-	// router.push(`/exams/update?examSessionId=${id}`);
-	router.push({ path: "ExamContent", query: { examId: examrealid } });
+  const examrealid = examSessionList.examrealid;
+  // router.push(`/exams/update?examSessionId=${id}`);
+  router.push({ path: "ExamContent", query: { examId: examrealid } });
 };
 </script>
 <style lang="scss" scoped>
 .case-wrapper {
-	width: 100%;
-	height: calc(100vh - 50px);
-	padding: 30px;
-	.case-container {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		.wrapper {
-			width: 100%;
-			margin-bottom: 10px;
-		}
-		.header-wrapper {
-			display: flex;
-			justify-content: center;
-			margin-bottom: 25px;
-			.search-form {
-				width: 800px;
-				height: 32px;
-			}
-		}
-		.btns-wrapper {
-			margin-left: 75px;
-			.btn-container {
-				display: inline-block;
-				margin-right: 7px;
-			}
-		}
-		.table-wrapper {
-			display: flex;
-			justify-content: center;
-			width: 100%;
-			.table-container {
-				width: 95%;
-			}
-		}
-	}
-}
-/deep/ .el-dialog {
-	width: 90% !important;
+  width: 100%;
+  height: calc(100vh - 50px);
+  padding: 30px;
+  .case-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    .wrapper {
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .header-wrapper {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 25px;
+      .search-form {
+        width: 800px;
+        height: 32px;
+      }
+    }
+    .btns-wrapper {
+      margin-left: 75px;
+      .btn-container {
+        display: inline-block;
+        margin-right: 7px;
+      }
+    }
+    .table-wrapper {
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      .table-container {
+        width: 95%;
+      }
+    }
+  }
 }
 </style>
--- pet-hospital-ui/src/views/System/Paper/PaperList.vue
+++ pet-hospital-ui/src/views/System/Paper/PaperList.vue
@@ -1,243 +1,240 @@
 <template>
-	<div class="case-wrapper">
-		<div class="case-container">
-			<!-- 模糊搜索框 -->
-			<div class="header-wrapper wrapper">
-				<el-form :inline="true" :model="paperlist" class="search">
-					<el-form-item label="试卷名称">
-						<el-input v-model="paperlist.papername" placeholder="试卷名称" />
-					</el-form-item>
-					<el-form-item>
-						<el-button type="primary" @click="onSubmit">搜索</el-button>
-						<el-button type="info" @click="resetForm">重置</el-button>
-					</el-form-item>
-				</el-form>
-			</div>
+  <div class="case-wrapper">
+    <div class="case-container">
+      <!-- 模糊搜索框 -->
+      <div class="header-wrapper wrapper">
+        <el-form :inline="true" :model="paperlist" class="search">
+          <el-form-item label="试卷名称">
+            <el-input v-model="paperlist.papername" placeholder="试卷名称" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">搜索</el-button>
+            <el-button type="info" @click="resetForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
 
-			<div class="btns-wrapper wrapper">
-				<div class="btn-container">
-					<router-link to="/paper/add">
-						<el-button class="AddButton" type="success" plain>新增</el-button>
-					</router-link>
-				</div>
-				<div class="btn-container"></div>
-				<div class="btn-container">
-					<el-button
-						class="DeleteButton"
-						@click="handleMultiDelete"
-						type="danger"
-						plain
-						>删除</el-button
-					>
-				</div>
-			</div>
-			<div class="table-wrapper wrapper">
-				<div class="table-container">
-					<el-table
-						ref="multipleTableRef"
-						:data="tableData"
-						height="500px"
-						:header-cell-style="{ 'text-align': 'center' }"
-						:cell-style="{ 'text-align': 'center' }"
-						@selection-change="handleSelectionChange"
-					>
-						<el-table-column type="selection" width="55" />
-						<el-table-column type="index" label="序号" width="55" />
+      <div class="btns-wrapper wrapper">
+        <div class="btn-container">
+          <router-link to="/paper/add">
+            <el-button class="AddButton" type="success" plain>新增</el-button>
+          </router-link>
+        </div>
+        <div class="btn-container"></div>
+        <div class="btn-container">
+          <el-button
+            class="DeleteButton"
+            @click="handleMultiDelete"
+            type="danger"
+            plain
+            >删除</el-button
+          >
+        </div>
+      </div>
+      <div class="table-wrapper wrapper">
+        <div class="table-container">
+          <el-table
+            ref="multipleTableRef"
+            :data="tableData"
+            height="500px"
+            :header-cell-style="{ 'text-align': 'center' }"
+            :cell-style="{ 'text-align': 'center' }"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55" />
+            <el-table-column type="index" label="序号" width="55" />
 
-						<el-table-column prop="papername" label="试卷名称" width="200" />
-						<el-table-column
-							prop="paperduration"
-							label="试卷时长"
-							width="130"
-						/>
-						<el-table-column
-							prop="papertotalscore"
-							label="试卷总分"
-							width="130"
-						/>
+            <el-table-column prop="papername" label="试卷名称" width="200" />
+            <el-table-column
+              prop="paperduration"
+              label="试卷时长"
+              width="130"
+            />
+            <el-table-column
+              prop="papertotalscore"
+              label="试卷总分"
+              width="130"
+            />
 
-						<el-table-column label="操作" width="320">
-							<template #default="scope">
-								<el-button
-									size="small"
-									type="info"
-									plain
-									@click="handleView(scope.row)"
-									>查看试卷内容</el-button
-								>
+            <el-table-column label="操作" width="320">
+              <template #default="scope">
+                <el-button
+                  size="small"
+                  type="info"
+                  plain
+                  @click="handleView(scope.row)"
+                  >查看试卷内容</el-button
+                >
 
-								<el-button
-									size="small"
-									type="warning"
-									plain
-									@click="handleEdit(scope.row)"
-									>编辑</el-button
-								>
-								<el-button
-									size="small"
-									type="danger"
-									plain
-									@click="handleDelete(scope.row)"
-									>删除</el-button
-								>
-							</template>
-						</el-table-column>
-					</el-table>
-				</div>
-			</div>
-		</div>
-	</div>
+                <el-button
+                  size="small"
+                  type="warning"
+                  plain
+                  @click="handleEdit(scope.row)"
+                  >编辑</el-button
+                >
+                <el-button
+                  size="small"
+                  type="danger"
+                  plain
+                  @click="handleDelete(scope.row)"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import { onMounted, reactive, ref } from "vue";
 import {
-	getPaperList,
-	deleteExaminationById,
-	getPaperByName,
+  getPaperList,
+  deleteExaminationById,
+  getPaperByName,
 } from "@/api/exam";
 import { useRouter } from "vue-router";
 const router = useRouter();
 const loading = ref();
 
-// let tableData = reactive([]);
+let tableData = ref([]);
 const paperlist = reactive({
-	examrealid: "",
+  examrealid: "",
 
-	paperid: "",
-	papername: "",
-	paperduration: "",
-	papertotalscore: "",
+  paperid: "",
+  papername: "",
+  paperduration: "",
+  papertotalscore: "",
 });
-const tableData = ref([]);
+const multipleTableRef = ref();
 
 onMounted(() => {
-	getAll();
+  getAll();
 });
 const handleSelectionChange = (val) => {
-	console.log(val);
+  console.log(val);
 };
 // 获取全部信息
 const getAll = async () => {
-	tableData.value = [];
-	loading.value = true;
-	let data = await getPaperList().then((res) => res.data);
-	data.examSessionList.forEach((item) => {
-		var value = {
-			examrealid: item.examSessionPaper.paperExam.examId,
-			paperid: item.examSessionPaper.paperId,
-			papername: item.examSessionPaper.paperName,
-			paperduration: item.examSessionPaper.paperDuration,
-			papertotalscore: item.examSessionPaper.paperTotalScore,
-		};
-		tableData.value.push(value);
-	});
-	console.log("tabledata", tableData);
+  tableData.value = [];
+  loading.value = true;
+  let data = await getPaperList().then((res) => res.data);
+  data.examSessionList.forEach((item) => {
+    var value = {
+      examrealid: item.examSessionPaper.paperExam.examId,
+      paperid: item.examSessionPaper.paperId,
+      papername: item.examSessionPaper.paperName,
+      paperduration: item.examSessionPaper.paperDuration,
+      papertotalscore: item.examSessionPaper.paperTotalScore,
+    };
+    tableData.value.push(value);
+  });
+  console.log("tabledata", tableData);
 };
 
 const handleDelete = (val) => {
-	console.log("val", val);
-	deletePaper(val.examrealid).then(() => {
-		tableData = reactive([]);
-		getAll();
-	});
+  console.log("val", val);
+  deletePaper(val.examrealid).then(() => {
+    tableData = reactive([]);
+    getAll();
+  });
 };
 // 删除接口
 const deletePaper = async (examrealid) => {
-	await deleteExaminationById(examrealid).then((res) =>
-		console.log("res", res)
-	);
+  await deleteExaminationById(examrealid).then((res) =>
+    console.log("res", res)
+  );
 };
 const onSubmit = async () => {
-	// console.log("submit!");
-	if (paperlist.papername === "") return;
-	tableData.value = [];
-	loading.value = true;
-	const data = await getPaperByName(paperlist.papername).then(
-		(res) => res.data
-	);
-	data.examSessionList.forEach((item) => {
-		var value = {
-			paperid: item.examSessionPaper.paperId,
-			papername: item.examSessionPaper.paperName,
-			paperduration: item.examSessionPaper.paperDuration,
-			papertotalscore: item.examSessionPaper.paperTotalScore,
-		};
-		tableData.value.push(value);
-	});
-	loading.value = false;
+  // console.log("submit!");
+  if (paperlist.papername === "") return;
+  tableData.value = [];
+  loading.value = true;
+  const data = await getPaperByName(paperlist.papername).then(
+    (res) => res.data
+  );
+  data.examSessionList.forEach((item) => {
+    var value = {
+      paperid: item.examSessionPaper.paperId,
+      papername: item.examSessionPaper.paperName,
+      paperduration: item.examSessionPaper.paperDuration,
+      papertotalscore: item.examSessionPaper.paperTotalScore,
+    };
+    tableData.value.push(value);
+  });
+  loading.value = false;
 
-	console.log("tabledata", tableData);
+  console.log("tabledata", tableData);
 };
 
 const handleMultiDelete = async () => {
-	let rows = multipleTableRef.value.getSelectionRows();
-	for (let item of rows) {
-		await deleteExaminationById(item.examrealid)
-			.then((res) => console.log("删除试卷成功", res))
-			.catch((err) => console.log("删除试卷失败", err));
-	}
-	getAll();
+  let rows = multipleTableRef.value.getSelectionRows();
+  for (let item of rows) {
+    await deleteExaminationById(item.examrealid)
+      .then((res) => console.log("删除试卷成功", res))
+      .catch((err) => console.log("删除试卷失败", err));
+  }
+  getAll();
 };
 const resetForm = () => {
-	paperlist.examrealid = "";
-	paperlist.paperid = "";
-	paperlist.papername = "";
-	paperlist.paperduration = "";
-	paperlist.papertotalscore = "";
+  paperlist.examrealid = "";
+  paperlist.paperid = "";
+  paperlist.papername = "";
+  paperlist.paperduration = "";
+  paperlist.papertotalscore = "";
 };
 
 const handleEdit = (examSessionList) => {
-	const paperid = examSessionList.paperid;
-	// router.push(`/exams/update?examSessionId=${id}`);
-	router.push({ path: "update", query: { paperId: paperid } });
+  const paperid = examSessionList.paperid;
+  // router.push(`/exams/update?examSessionId=${id}`);
+  router.push({ path: "update", query: { paperId: paperid } });
 };
 
 const handleView = (examSessionList) => {
-	const paperid = examSessionList.paperid;
-	// router.push(`/exams/update?examSessionId=${id}`);
-	router.push({ path: "content", query: { paperId: paperid } });
+  const paperid = examSessionList.paperid;
+  // router.push(`/exams/update?examSessionId=${id}`);
+  router.push({ path: "content", query: { paperId: paperid } });
 };
 </script>
 <style lang="scss" scoped>
 .case-wrapper {
-	width: 100%;
-	height: calc(100vh - 50px);
-	padding: 30px;
-	.case-container {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		.wrapper {
-			width: 100%;
-			margin-bottom: 10px;
-		}
-		.header-wrapper {
-			display: flex;
-			justify-content: center;
-			margin-bottom: 25px;
-			.search-form {
-				width: 800px;
-				height: 32px;
-			}
-		}
-		.btns-wrapper {
-			margin-left: 75px;
-			.btn-container {
-				display: inline-block;
-				margin-right: 7px;
-			}
-		}
-		.table-wrapper {
-			display: flex;
-			justify-content: center;
-			width: 100%;
-			.table-container {
-				width: 95%;
-			}
-		}
-	}
-}
-/deep/ .el-dialog {
-	width: 90% !important;
+  width: 100%;
+  height: calc(100vh - 50px);
+  padding: 30px;
+  .case-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    .wrapper {
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    .header-wrapper {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 25px;
+      .search-form {
+        width: 800px;
+        height: 32px;
+      }
+    }
+    .btns-wrapper {
+      margin-left: 75px;
+      .btn-container {
+        display: inline-block;
+        margin-right: 7px;
+      }
+    }
+    .table-wrapper {
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      .table-container {
+        width: 95%;
+      }
+    }
+  }
 }
 </style>
